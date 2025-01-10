2c599c5be4ec2ae78db19dc80e629c27b1ae0098
Pratham Panchal <102433807+pratham0612@users.noreply.github.com>
2025-01-10 03:04:18 -0500

    Update Languages of the meter

--- components/Common/Intro/Languages.jsx
+++ components/Common/Intro/Languages.jsx
@@ -2,14 +2,14 @@ import { Progress } from "antd"
 import { useEffect, useState } from "react"
 
 const Languages = () => {
-    const [urdu, setUrdu] = useState(0)
+    const [hindi, setHindi] = useState(0)
     const [english, setEnglish] = useState(0)
     const [gujarati, setGujarati] = useState(0)
 
     useEffect(() => {
         const timer = setInterval(() => {
-            if (urdu < 98) {
-                setUrdu(prevCount => prevCount + 1);
+            if (hindi < 98) {
+                setHindi(prevCount => prevCount + 1);
             }
             if (english < 88) {
                 setEnglish(prevCount => prevCount + 1);
@@ -20,14 +20,14 @@ const Languages = () => {
         }, 5);
 
         return () => clearInterval(timer);
-    }, [urdu, english])
+    }, [hindi, english])
     return (
         <div className="flex flex-col space-y-1 pt-6">
             <div className="flex flex-col gap-y-4">
                 <span className='text-Snow text-xs font-bold'>Languages</span>
                 <div className="flex flex-row items-center justify-between gap-4 flex-wrap">
                     <div className="flex flex-col items-center justify-center gap-y-2">
-                        <Progress strokeColor="#DFAC1F" type="circle" percent={urdu} size={50} />
+                        <Progress strokeColor="#DFAC1F" type="circle" percent={hindi} size={50} />
                         <span className='text-xs font-bold text-Snow'>Hindi</span>
                     </div>
                     <div className="flex flex-col items-center justify-center gap-y-2">
